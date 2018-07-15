0e596c94930f53f2304386c8b8d89cd60d901a21
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-15 14:25:18 -0300

    testes

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -1,60 +1,29 @@
-package br.com.br1000apps.AdMobAdview;
+package com.br.br1000apps.plugins.AdMobAdviewPlugin;
 
-import java.util.HashMap;
-import java.util.Iterator;
-
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.location.Location;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
+import android.content.Context;
+import android.widget.Toast;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
-import com.google.android.gms.ads.doubleclick.PublisherAdView;
-import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
-import com.google.android.gms.ads.mediation.admob.AdMobExtras;
-import com.google.android.gms.ads.reward.RewardedVideoAd;
-import com.google.android.gms.ads.reward.RewardedVideoAdListener;
-import com.google.android.gms.ads.reward.RewardItem;
-import com.google.ads.mediation.admob.AdMobAdapter;
+public class MyToast extends CordovaPlugin {
 
-import java.lang.reflect.Method;
-import java.lang.NoSuchMethodException;
-import java.util.ArrayList;
+    public MyToast() {
 
-import org.apache.cordova.CallbackContext;
-
-public class AdMobAdviewPlugin extends CordovaPlugin {
-
-@Override
-public boolean execute(String action, JSONArray args, CallbackContext callbackContext){
-    try {
-        if (PluginsName.HELLO_WORLD.equals(action)){
-                helloWorld(args, callbackContext);
-                return true;
-        }
-    } catch (Exception e) {
-        callbackContext.error(e.getMessage());
-        return false;
     }
-    return false;
-}
 
-public void helloWorld(JSONArray args, CallbackContext callbackContext) throws Exception{
-    callbackContext.success("Hello World");
-}
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
 
-}
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
+    }
+}
