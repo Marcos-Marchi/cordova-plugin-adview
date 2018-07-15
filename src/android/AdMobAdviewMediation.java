e55f9e55119a22cc70bce1bb9bc37584b4b1a95a
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-15 13:33:02 -0300

    testes

--- src/android/AdMobAdviewMediation.java
+++ src/android/AdMobAdviewMediation.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import com.google.android.gms.ads.AdRequest;
 
 public abstract class AdMobadviewMediation {
-	public AdMobadviewMediation(Activity act, JSONObject options) {}
+	public AdMobMediation(Activity act, JSONObject options) {}
 	public abstract AdRequest.Builder joinAdRequest(AdRequest.Builder builder);
 	public void onPause(){}
 	public void onResume(){}
