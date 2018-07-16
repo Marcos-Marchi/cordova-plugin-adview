b76774da99d74ec2b37c10c8e11155f8176de87d
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 11:09:06 -0300

    TESTES

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -1,19 +1,18 @@
 package com.br.br1000apps.plugins.AdMobAdviewPlugin;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
-    private AdView mAdView;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MobileAds.initialize(this,
-            "ca-app-pub-3940256099942544~3347511713");
-
-        mAdView = findViewById(R.id.adView);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
     }
