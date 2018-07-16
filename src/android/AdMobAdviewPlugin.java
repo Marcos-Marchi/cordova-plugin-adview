f1203dbf2d7059fac158615c76df3077496471de
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 19:03:08 -0300

    teste

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -4,10 +4,13 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class AdMobAdviewPlugin extends CordovaActivty {
+public class AdMobAdviewPlugin extends AppCompatActivity {
 
 	private AdView mAdView;
 
