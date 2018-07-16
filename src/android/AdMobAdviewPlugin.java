0be878de8b98895263a1079ed711c62ba0a4e5ac
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 18:35:53 -0300

    tstesre

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -4,29 +4,12 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.support.v7.app.AppCompatActivity;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class AdMobAdviewPlugin extends CordovaPlugin {
+public class AdMobAdviewPlugin extends extends AppCompatActivity {
 
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("greet")) {
-
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-
-            return true;
-
-        } else {
-            
-            return false;
-
-        }
-    }
-	
 	private AdView mAdView;
 
     protected void onCreate(Bundle savedInstanceState) {
