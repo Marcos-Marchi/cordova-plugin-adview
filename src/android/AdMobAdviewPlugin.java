7a444e5aa051b8eb2b63fa09a46b72ba000e0a42
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 11:28:18 -0300

    TESTES

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -26,44 +26,11 @@ public class AdMobAdviewPlugin extends GenericAdPlugin {
   private AdView adView;
 
   @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    // Create the adView.
-    adView = new AdView(this);
-    adView.setAdUnitId(MY_AD_UNIT_ID);
-    adView.setAdSize(AdSize.BANNER);
-
-    // Lookup your LinearLayout assuming it's been given
-    // the attribute android:id="@+id/mainLayout".
-    LinearLayout layout = (LinearLayout)findViewById(R.id.mainLayout);
-
-    // Add the adView to it.
-    layout.addView(adView);
-
-    // Initiate a generic request.
-    AdRequest adRequest = new AdRequest.Builder().build();
-
-    // Load the adView with the ad request.
-    adView.loadAd(adRequest);
+  protected void pluginInitialize() {
+    super.pluginInitialize();
+    
+    // TODO: any init code
   }
-
-  @Override
-  public void onPause() {
-    adView.pause();
-    super.onPause();
-  }
-
-  @Override
-  public void onResume() {
-    super.onResume();
-    adView.resume();
-  }
-
-  @Override
-  public void onDestroy() {
-    adView.destroy();
-    super.onDestroy();
-   }
- }
+  
+  
+}
