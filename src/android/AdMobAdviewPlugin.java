08222473acf177803501d65a18506d3899af4244
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 18:42:17 -0300

    teste

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class AdMobAdviewPlugin extends AppCompatActivity {
+public class AdMobAdviewPlugin extends CordovaPlugin {
 
 	private AdView mAdView;
 
