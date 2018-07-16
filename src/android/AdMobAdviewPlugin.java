e53c92a3bf421b9f9e9e8812a993e6bdeaa2bb20
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 18:32:08 -0300

    testes

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -4,6 +4,9 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class AdMobAdviewPlugin extends CordovaPlugin {
 
     @Override
@@ -23,4 +26,20 @@ public class AdMobAdviewPlugin extends CordovaPlugin {
 
         }
     }
+	
+	private AdView mAdView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this,
+            "ca-app-pub-3940256099942544~3347511713");
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+	
+	
 }
