4da40c21e179e96a6f5e3289e6588e3bf08283bd
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-16 11:16:56 -0300

    TESTES

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -1,8 +1,25 @@
 package com.br.br1000apps.plugins.AdMobAdviewPlugin;
 
-import com.google.android.gms.ads.*;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
+import com.google.android.gms.ads.mediation.admob.AdMobExtras;
+import com.google.android.gms.ads.reward.RewardedVideoAd;
+import com.google.android.gms.ads.reward.RewardedVideoAdListener;
+import com.google.android.gms.ads.reward.RewardItem;
+import com.google.ads.mediation.admob.AdMobAdapter;
 
-  public class BannerExample extends Activity {
+import java.lang.reflect.Method;
+import java.lang.NoSuchMethodException;
+import java.util.ArrayList;
+
+public class BannerExample extends Activity {
   private AdView adView;
 
   @Override
