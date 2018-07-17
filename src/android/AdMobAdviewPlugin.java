bce9204681cad9e7834cc9b115dc701a7d0f4e50
Marcos-Marchi <41209004+Marcos-Marchi@users.noreply.github.com>
2018-07-17 14:37:23 -0300

    TESTES

--- src/android/AdMobAdviewPlugin.java
+++ src/android/AdMobAdviewPlugin.java
@@ -4,23 +4,10 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import com.br1000apps.APP000001.R;
-import android.os.Build;
+import android.*;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.location.Location;
-import android.os.Bundle;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
+import com.google.android.gms.ads.*;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.MobileAds;
 
 public class AdMobAdviewPlugin extends Activity {
 
